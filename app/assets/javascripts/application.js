c08c022ae25b6dab5036274bb2968921db491932
mkkoala <mako1105.96@gmail.com>
2020-02-19 09:36:56 +0000

    [Update] レイアウトの更新

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,7 +14,6 @@
 //= require activestorage
 //= require jquery
 //= require bootstrap-sprockets
-//= require_tree ../../../app/assets/javascripts/.
 //= require_tree .
 
 
@@ -27,7 +26,7 @@ $("#theTarget").skippr({
     // スライドショーの変化 ("fade" or "slide")
     transition : 'slide',
     // 変化に係る時間(ミリ秒)
-    speed : 1000,
+    speed : 2000,
     // easingの種類
     easing : 'easeOutQuart',
     // ナビゲーションの形("block" or "bubble")
@@ -39,7 +38,7 @@ $("#theTarget").skippr({
     // スライドショーの自動再生(falseで自動再生なし)
     autoPlay : true,
     // 自動再生時のスライド切替間隔(ミリ秒)
-    autoPlayDuration : 1000,
+    autoPlayDuration : 2000,
     // キーボードの矢印キーによるスライド送りの設定(trueで有効)
     keyboardOnAlways : true,
     // 一枚目のスライド表示時に戻る矢印を表示するかどうか(falseで非表示)

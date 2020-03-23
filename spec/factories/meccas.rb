c647a1a40d87ecabd8109cf13900fa1e254810f0
mkkoala <mako1105.96@gmail.com>
2020-03-23 10:46:41 +0000

    [Update]RSpecテスト　Faker引数の修正

--- spec/factories/meccas.rb
+++ spec/factories/meccas.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :mecca do
     sequence(:name) { |n| "MECCA_NAME#{n}"}
     sequence(:body) { |n| "BODY_#{n}"}
-    sequence(:mecca_image_id) { Faker::Lorem.characters(60) }
+    sequence(:mecca_image_id) { Faker::Lorem.characters(number: 60) }
     sequence(:address) { |n| "ADDRESS#{n}"}
     sequence(:post_code) { |n| "123456#{n}"}
   end
